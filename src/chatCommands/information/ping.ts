f88debeb9a9259fa147ad4a5c3ac01a00bd444ea
RenaOfficial <y.games@outlook.jp>
2023-12-16 14:23:54 +0900

    Add チャットコマンドにPINGを追加

--- src/chatCommands/information/ping.ts
+++ src/chatCommands/information/ping.ts
@@ -1,8 +1,35 @@
 import { ChatCommand } from '@/lib/classes/ChatCommand';
+import { Colors } from 'discord.js';
+import { footer } from '@/lib/functions/Embed';
+import { client } from '@/index';
 
 export default new ChatCommand({
   name: 'ping',
-  run: ({ message }) => {
-    message.reply('pong');
+  run: async ({ message }) => {
+    const msg = await message.reply({
+      embeds: [
+        {
+          title: '🏓 Pinging...',
+          color: Colors.Red,
+          footer: footer(),
+        },
+      ],
+      allowedMentions: {
+        parse: []
+      }
+    })
+
+    const latency = new Date().getTime() - msg.createdTimestamp;
+
+    await msg.edit({
+      embeds: [
+        {
+          title: '🏓 Pong!',
+          description: `Client Latency: ${client.ws.ping}ms\nMessage Edit Latency: ${latency}ms`,
+          color: Colors.Aqua,
+          footer: footer(),
+        },
+      ],
+    })
   },
 });
