c084435f3d534e13f717390ba7cbb0a23863fcd3
RenaOfficial <y.games@outlook.jp>
2023-12-16 17:27:21 +0900

    Fix Pingコマンドを共通

--- src/chatCommands/information/ping.ts
+++ src/chatCommands/information/ping.ts
@@ -2,6 +2,7 @@ import { ChatCommand } from '@/lib/classes/ChatCommand';
 import { Colors } from 'discord.js';
 import { footer } from '@/lib/functions/Embed';
 import { client } from '@/index';
+import osu from 'node-os-utils';
 
 export default new ChatCommand({
   name: 'ping',
@@ -21,11 +22,36 @@ export default new ChatCommand({
 
     const latency = new Date().getTime() - msg.createdTimestamp;
 
+    const cpuUsage = await osu.cpu.usage();
+    const memUsage = (await osu.mem.info()).usedMemPercentage;
+
+    const resources = [
+      '__<:ram:1185464482349191168> **RAM: **' + `\`${memUsage}%\``,
+      '__<:loading:1185464479107002368> **CPU: **' + `\`${cpuUsage}%\``,
+    ];
+
+    const fields = [
+      '_<:latency:1185464475692826624> **Latency: **' +
+        `\`${client.ws.ping}ms\``,
+      '_<:cooldown:1185464473696337940>  **Edit Latency: **' +
+        `\`${latency}ms\``,
+      `_<:trade:1185464469577535578> **Resources: **\n${resources
+        .map((msg) => msg.replace(' ', '<:space:1185464484685422652>'))
+        .join('\n')}`,
+    ];
+
     await msg.edit({
       embeds: [
         {
-          title: '🏓 Pong!',
-          description: `Client Latency: ${client.ws.ping}ms\nMessage Edit Latency: ${latency}ms`,
+          title: 'Bot Status:',
+          fields: [
+            {
+              name: '<:shard:1185465377782771852> Shard [0]:',
+              value: fields
+                .map((msg) => msg.replace(' ', '<:space:1185464484685422652>'))
+                .join('\n'),
+            },
+          ],
           color: Colors.Aqua,
           footer: footer(),
         },
--- src/commands/information/ping.ts
+++ src/commands/information/ping.ts
@@ -1,6 +1,8 @@
 import { ApplicationCommandType, Colors } from 'discord.js';
 import { Command } from '@/lib/classes/Command';
 import { footer } from '@/lib/functions/Embed';
+import osu from 'node-os-utils';
+import { client } from '@/index';
 
 export default new Command({
   name: 'ping',
@@ -21,11 +23,36 @@ export default new Command({
     const latency =
       new Date().getTime() - (await interaction.fetchReply()).createdTimestamp;
 
+    const cpuUsage = await osu.cpu.usage();
+    const memUsage = (await osu.mem.info()).usedMemPercentage;
+
+    const resources = [
+      '__<:ram:1185464482349191168> **RAM: **' + `\`${memUsage}%\``,
+      '__<:loading:1185464479107002368> **CPU: **' + `\`${cpuUsage}%\``,
+    ];
+
+    const fields = [
+      '_<:latency:1185464475692826624> **Latency: **' +
+        `\`${client.ws.ping}ms\``,
+      '_<:cooldown:1185464473696337940>  **Edit Latency: **' +
+        `\`${latency}ms\``,
+      `_<:trade:1185464469577535578> **Resources: **\n${resources
+        .map((msg) => msg.replace(' ', '<:space:1185464484685422652>'))
+        .join('\n')}`,
+    ];
+
     await interaction.editReply({
       embeds: [
         {
-          title: '🏓 Pong!',
-          description: `Client Latency: ${client.ws.ping}ms\nMessage Edit Latency: ${latency}ms`,
+          title: 'Bot Status:',
+          fields: [
+            {
+              name: '<:shard:1185465377782771852> Shard [0]:',
+              value: fields
+                .map((msg) => msg.replace(' ', '<:space:1185464484685422652>'))
+                .join('\n'),
+            },
+          ],
           color: Colors.Aqua,
           footer: footer(),
         },
