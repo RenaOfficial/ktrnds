0c20dcdfa2ae0ebb78fa2f790706add73a3fc264
RenaOfficial <y.games@outlook.jp>
2023-12-16 12:10:33 +0900

    Feat ダッシュボード用のAPIを作成

--- src/server/index.ts
+++ src/server/index.ts
@@ -8,56 +8,112 @@ export function server() {
   app.use(express.json());
 
   app.get('/', (req: Request, res: Response) => {
+    res.header('Access-Control-Allow-Origin', '*');
     try {
       // ボットが既にログインしている場合は再接続
       if (!client.isReady()) {
-        res.json({ status: 'success', message: 'Bot is not ready' });
+        res.json({
+          status: true,
+          ready: false,
+          message: 'Bot is not ready',
+        });
       } else {
-        res.json({ status: 'success', message: 'Bot is ready' });
+        res.json({
+          status: true,
+          ready: true,
+          message: 'Bot is ready',
+        });
       }
     } catch (error) {
-      console.error(error);
-      res.json({ status: 'error', message: 'Failed to retrieve Bot status.' });
+      res.json({
+        status: false,
+        ready: false,
+        message: error,
+      });
     }
   });
 
   app.post('/actions/start', (req: Request, res: Response) => {
     try {
-      // ボットが既にログインしている場合は再接続
       if (!client.isReady()) {
-        client.login(process.env.CLIENT_TOKEN);
+        client.login(process.env.CLIENT_TOKEN).then(() => {
+          console.log(
+            '\x1b[33mStart the bot upon request from the dashboard\x1b[0m'
+          );
+          res.json({
+            status: true,
+            message: 'Bot started successfully',
+          });
+        });
       }
-      res.json({ status: 'success', message: 'Bot started successfully' });
+      res.json({
+        status: true,
+        message: 'Bot started successfully',
+      });
     } catch (error) {
-      console.error(error);
-      res.json({ status: 'error', message: 'Failed to start the bot' });
+      res.json({
+        status: false,
+        message: error,
+      });
     }
   });
 
   app.post('/actions/restart', (req: Request, res: Response) => {
     try {
-      // ボットがログインしている場合はログアウトして再接続
       if (client.isReady()) {
-        client.destroy();
+        console.log(
+          '\x1b[33mRestart the bot upon request from the dashboard\x1b[0m'
+        );
+        client.destroy().then(() => {
+          client.login(process.env.CLIENT_TOKEN).then(() => {
+            res.json({
+              status: true,
+              message: 'Bot restarted successfully',
+            });
+          });
+        });
+      } else {
+        console.log(
+          '\x1b[33mRestart(Start) the bot upon request from the dashboard\x1b[0m'
+        );
+        client.login(process.env.CLIENT_TOKEN).then(() => {
+          res.json({
+            status: true,
+            message: 'Bot restarted successfully',
+          });
+        });
       }
-      client.login(process.env.CLIENT_TOKEN);
-      res.json({ status: 'success', message: 'Bot restarted successfully' });
     } catch (error) {
-      console.error(error);
-      res.json({ status: 'error', message: 'Failed to restart the bot' });
+      res.json({
+        status: false,
+        message: error,
+      });
     }
   });
 
   app.post('/actions/stop', (req: Request, res: Response) => {
     try {
-      // ボットがログインしている場合はログアウト
       if (client.isReady()) {
-        client.destroy();
+        console.log(
+          '\x1b[33mStop the bot upon request from the dashboard\x1b[0m'
+        );
+        client.destroy().then(() => {
+          res.json({
+            status: true,
+            message: 'Bot stopped successfully',
+          });
+        });
+      } else {
+        res.json({
+          status: true,
+          message: 'Bot stopped successfully',
+        });
       }
-      res.json({ status: 'success', message: 'Bot stopped successfully' });
     } catch (error) {
-      console.error(error);
-      res.json({ status: 'error', message: 'Failed to stop the bot' });
+      res.json({
+        status: false,
+        message: error,
+      });
     }
   });
 
