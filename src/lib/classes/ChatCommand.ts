2942cbe9c7a33756197fee1cb559532443146709
RenaOfficial <y.games@outlook.jp>
2023-12-19 17:48:30 +0900

    Add ToSの埋め込みのJSONデータをアップロード

--- src/lib/classes/ChatCommand.ts
+++
@@ -1,7 +0,0 @@
-import { ChatCommandType } from '@/lib/interfaces/ChatCommand';
-
-export class ChatCommand {
-  constructor(commandOptions: ChatCommandType) {
-    Object.assign(this, commandOptions);
-  }
-}
