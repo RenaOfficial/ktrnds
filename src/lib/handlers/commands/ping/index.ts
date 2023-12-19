f2b36b0bc1991a9da65af89e37799b3e97a605b1
RenaOfficial <y.games@outlook.jp>
2023-12-20 00:56:27 +0900

    Refactor スペースの表示方法の改善

--- src/lib/handlers/commands/ping/index.ts
+++ src/lib/handlers/commands/ping/index.ts
@@ -20,16 +20,20 @@ async function sendReply({
   const memUsage = (await osu.mem.info()).usedMemPercentage;
 
   const resources = [
-    '-<:space:1185464484685422652><:ram:1185464482349191168> **RAM: **' +
+    '<:space:1185464484685422652><:space:1185464484685422652><:ram:1185464482349191168> **RAM: **' +
       `\`${memUsage}%\``,
-    '-<:space:1185464484685422652><:loading:1185464479107002368> **CPU: **' +
+    '<:space:1185464484685422652><:space:1185464484685422652><:loading:1185464479107002368> **CPU: **' +
       `\`${cpuUsage}%\``,
   ];
 
   const fields = [
-    '-<:latency:1185464475692826624> **Latency: **' + `\`${client.ws.ping}ms\``,
-    '-<:cooldown:1185464473696337940>  **Edit Latency: **' + `\`${latency}ms\``,
-    `-<:trade:1185464469577535578> **Resources: **\n${resources.join('\n')}`,
+    '<:space:1185464484685422652><:latency:1185464475692826624> **Latency: **' +
+      `\`${client.ws.ping}ms\``,
+    '<:space:1185464484685422652><:cooldown:1185464473696337940>  **Edit Latency: **' +
+      `\`${latency}ms\``,
+    `<:space:1185464484685422652><:trade:1185464469577535578> **Resources: **\n${resources.join(
+      '\n'
+    )}`,
   ];
 
   return {
@@ -39,9 +43,7 @@ async function sendReply({
         fields: [
           {
             name: '<:shard:1185465377782771852> Shard [0]:',
-            value: fields
-              .map((msg) => msg.replace('-', '<:space:1185464484685422652>'))
-              .join('\n'),
+            value: fields.join('\n'),
           },
         ],
         color: Colors.Aqua,
