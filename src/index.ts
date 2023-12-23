26e69265d21c2e9ddf58baab3edda6ef9229f90b
RenaOfficial <y.games@outlook.jp>
2023-12-24 01:57:37 +0900

    Chore エラー落ちを回避

--- src/index.ts
+++ src/index.ts
@@ -5,3 +5,11 @@ export const client = new ExtendedClient();
 
 console.clear();
 client.start();
+
+process.on("uncaughtException", async (error: Error) => {
+  return error
+})
+
+process.on('unhandledRejection', async (error: Error) => {
+  return error;
+});
